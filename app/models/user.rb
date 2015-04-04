6526f5ca850a776b6e0b4362fb9c5fc46ae099ba
Tho Nguyen <thogotmail@gmail.com>
2015-04-04 05:51:40 +0000

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :location_sessions
 	has_many :relationships
+	
+	validates_presence_of :account_name, :display_name, :registration_id
+	validates_uniqueness_of :account_name, :registration_id
 end
